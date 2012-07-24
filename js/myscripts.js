885dce22d3c21b92ed3cf1a686a2ae5c1a84e69c
unknown <kuryleva2@gmail.com>
2012-07-24 18:14:43 +0400

    strelka

--- js/myscripts.js
+++ js/myscripts.js
@@ -8,6 +8,30 @@ $("#m").click(function(){
     $("body,html").animate({"scrollTop":'1294'},800);
 });
 
+$("#t").click(function(){
+    //Необходимо прокрутить в конец страницы
+    var curPos = $(document).scrollTop();
+    var height = $("body").height();
+    var scrollTime = (height-curPos)/1.73;
+    $("body,html").animate({"scrollTop":'680'},800);
+});
+
+$("#k").click(function(){
+    //Необходимо прокрутить в конец страницы
+    var curPos = $(document).scrollTop();
+    var height = $("body").height();
+    var scrollTime = (height-curPos)/1.73;
+    $("body,html").animate({"scrollTop":'1909'},800);
+});
+
+$("#c").click(function(){
+    //Необходимо прокрутить в конец страницы
+    var curPos = $(document).scrollTop();
+    var height = $("body").height();
+    var scrollTime = (height-curPos)/1.73;
+    $("body,html").animate({"scrollTop":'1909'},800);
+});
+
 });
 
 /* //Обработка нажатия на кнопку "Вверх"
